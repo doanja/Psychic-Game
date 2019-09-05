2bfd45c817e5d7ff62638bca2645801c385fccca
John Doan <doanja@plu.edu>
2019-09-05 08:59:45 -0700

    added bootstrap for index.html, initialize variables for game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+let numberOfGuesses = 0; // counter for number of guesses
+let lives = 2; // counter for lives
+let guessedLetters = []; // array to keep track of each leter guessed
+
+const wordBank = [
+  'monkey',
+  'bear',
+  'elephant',
+  'gorilla',
+  'dog',
+  'cat',
+  'cow',
+  'pig',
+  'goat',
+  'horse'
+]; // array containing available words the player can guess
+
+// array containing every lowercase letter in the English alphabet
+const letterBank = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
